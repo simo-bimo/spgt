2ba613790b687ad3f874ae11368c97dab456bfcf
simo-bimo <tinkeretan@gmail.com>
2025-10-07 18:17:06 +1100

    First proper version of PPLTL planner, with PPLTL regression. Includes a slightly scrappy method to keep track of the number of times a formula is regressed, to avoid regressing since operators indefinitely.

--- src/spgt/names.py
+++ src/spgt/names.py
@@ -2,7 +2,11 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 ASP_CODE_DIR = os.path.abspath(os.path.join(ROOT_DIR, "asp"))
+
 ASP_PLANNER_PATH = os.path.abspath(os.path.join(ASP_CODE_DIR, "regression_variable_planner.lp"))
-ASP_REGRESSOR_PATH  = os.path.abspath(os.path.join(ASP_CODE_DIR, "regressor_variable.lp"))
-ASP_CLINGRAPH_PATH = os.path.abspath(os.path.join(ASP_CODE_DIR, "clingraph_generator.lp"))
 ASP_PPLTL_PLANNER_PATH = os.path.abspath(os.path.join(ASP_CODE_DIR, "reg_var_ppltl_planner.lp"))
+
+ASP_REGRESSOR_PATH  = os.path.abspath(os.path.join(ASP_CODE_DIR, "regressor_variable.lp"))
+ASP_PPLTL_REGRESSOR_PATH  = os.path.abspath(os.path.join(ASP_CODE_DIR, "regressor_var_ppltl.lp"))
+
+ASP_CLINGRAPH_PATH = os.path.abspath(os.path.join(ASP_CODE_DIR, "clingraph_generator.lp"))
